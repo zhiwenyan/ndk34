4cda4d2a402f54c640751f49f7a5749a42335099
zhiwenyan <zhiwenyan@icloud.com>
2018-09-28 17:52:44 +0800

    双向链表

--- app/src/main/cpp/LinkedList.hpp
+++ app/src/main/cpp/LinkedList.hpp
@@ -12,12 +12,14 @@ template<class E>
 
 struct Node {
     Node<E> *next;
+    Node<E> *prev;
     E value;
 
 public:
-    Node(E value, Node<E> *next) {
-        this->next = next;
+    Node(E value, Node<E> *prev, Node<E> *next) {
         this->value = value;
+        this->prev = prev;
+        this->next = next;
     }
 };
 
@@ -47,34 +49,70 @@ public:
      */
     int size();
 
+    /**
+     *
+     * @param index
+     * @return
+     */
     E get(int index);
 
 
+    /**
+     *
+     * @param index
+     * @param e
+     */
     void insert(int index, E e);
 
-    void remove(int index);
+    /**
+     *
+     * @param index
+     */
+    E remove(int index);
 
     ~LinkedList();
 
+    /**
+     * 查找当前节点
+     * @param i
+     * @return
+     */
     Node<E> *node(int i);
+
+    /**
+     *
+     * @param e
+     */
+    void linkLast(E e);
+
+    /**
+     *
+     * @param cur
+     */
+    void linkBefore(Node<E> *cur, E e);
+
+
+    E unlink(Node<E> *cur);
 };
 
 template<class E>
 void LinkedList<E>::push(E e) {
     //添加一个数据
-    Node<E> *new_node = new Node<E>(e, NULL);
+    linkLast(e);
+    len++;
+    /*Node<E> *new_node = new Node<E>(e, NULL);
     if (head) {
         // head->next = new_node;
         //找到尾巴节点，有一个特点就是next 是为空
-        /* Node<E> *h = head;
-         while (h) {
-             if (h->next == NULL) {
-                 break;
-             }
-             h = h->next;
-         }
-         h->next = new_node;
-         */
+//        Node<E> *h = head;
+//         while (h) {
+//             if (h->next == NULL) {
+//                 break;
+//             }
+//             h = h->next;
+//         }
+//         h->next = new_node;
+//
         //每次node(len-1)的时间复杂O(n)
         //如何优化？？？
         //记录last节点 尾节点 时间复杂度变成O(1)
@@ -82,10 +120,10 @@ void LinkedList<E>::push(E e) {
         last->next = new_node;
     } else {
         head = new_node;
-        last = new_node;
     }
     last = new_node;
-    len++;
+    len++;*/
+
 }
 
 template<class E>
@@ -103,11 +141,30 @@ E LinkedList<E>::get(int index) {
 
 template<class E>
 Node<E> *LinkedList<E>::node(int index) {
-    Node<E> *h = head;
-    for (int i = 0; i < index; ++i) {
-        h = h->next;
+    //时间复杂度是O(n)级别
+    /*   Node<E> *h = head;
+       for (int i = 0; i < index; ++i) {
+           h = h->next;
+       }
+
+
+       return h;*/
+    //时间复杂度是O(n/2)级别
+    if (index < len >> 1) {
+        // 从前往后变量
+        Node<E> *cur = head;
+        for (int i = 0; i < index; ++i) {
+            cur = cur->next;
+        }
+        return cur;
+    } else {
+        // 从后往前遍历
+        Node<E> *cur = last;
+        for (int i = len - 1; i > index; i--) {
+            cur = cur->prev;
+        }
+        return cur;
     }
-    return h;
 }
 
 
@@ -115,7 +172,15 @@ template<class E>
 void LinkedList<E>::insert(int index, E e) {
     assert(index >= 0 && index <= len);
     //考虑边界问题index=0的情况
-    Node<E> *new_node = new Node<E>(e, NULL);
+    // 考虑边界
+    if (index == len) {
+        linkLast(e);
+    } else {
+        linkBefore(node(index), e);
+    }
+    len++;
+
+/*    Node<E> *new_node = new Node<E>(e, NULL);
     if (index == 0) {
         Node<E> *h = head;
         head = new_node;
@@ -128,14 +193,16 @@ void LinkedList<E>::insert(int index, E e) {
         new_node->next = next;
     }
     len++;
-
+*/
 }
 
 template<class E>
-void LinkedList<E>::remove(int index) {
+E LinkedList<E>::remove(int index) {
     assert(index >= 0 && index < len);
+    return unlink(node(index));
+
     //考虑边界问题 0 len mid
-    if (index == 0) {
+/*    if (index == 0) {
         Node<E> *h = head;
         head = h->next;
         //释放
@@ -148,14 +215,78 @@ void LinkedList<E>::remove(int index) {
         prev->next = cur->next;
 
         delete cur;
+    }*/
+
+}
+
+
+template<class E>
+void LinkedList<E>::linkLast(E e) {
+    Node<E> *l = last;
+    Node<E> *new_node = new Node<E>(e, l, NULL);
+    last = new_node;
+    if (head) {
+        l->next = new_node;
+    } else {
+        head = new_node;
     }
-    len--;
 
 }
 
 template<class E>
 LinkedList<E>::~LinkedList() {
     // 析构释放内存 ？
+    Node<E> *h;
+    h = head;
+    while (h) {
+        Node<E> *next = h->next;
+        delete h;
+        h = next;
+    }
+    //头指针以及尾指针也要置空
+    head = NULL;
+    last = NULL;
+
+
 }
+
+template<class E>
+void LinkedList<E>::linkBefore(Node<E> *cur, E e) {
+    Node<E> *prev = cur->prev;
+    Node<E> *new_node = new Node<E>(e, prev, cur);
+
+    //当前的上一个节点指向新增的节点
+    cur->prev = new_node;
+    if (prev) {
+        //上一个节点的next指向新增的节点
+        prev->next = new_node;
+    } else {
+        head = new_node;
+    }
+}
+
+template<class E>
+E LinkedList<E>::unlink(Node<E> *cur) {
+    //左右两个节点
+    Node<E> *prev = cur->prev;
+    Node<E> *next = cur->next;
+
+    E value = cur->value;
+    //考虑左右两边为空的情况，严谨，思维灵活
+    if (prev) {
+        prev->next = next;
+    } else {
+        head = next;
+    }
+    if (next) {
+        next->prev = prev;
+    } else {
+        last = prev;
+    }
+    delete cur;
+    len--;
+    return value;
+}
+
 
 #endif //NDK34_LINKEDLIST_HPP
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,31 +16,33 @@ Java_com_steven_ndk34_MainActivity_stringFromJNI(
 
 
     LinkedList<int> linkedList;
-//
-    /* linkedList.push(0);
-     linkedList.push(1);
-     linkedList.push(2);
-     linkedList.push(3);
-     linkedList.push(4);
-     linkedList.push(5);
-     linkedList.insert(0, -1);
-     linkedList.insert(2, 8);
-     linkedList.insert(linkedList.size(), 10);
 
-     linkedList.remove(0);
-     linkedList.remove(linkedList.size() - 1);
-     linkedList.remove(2);
-     for (int i = 0; i < linkedList.size(); ++i) {
-         __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", linkedList.get(i));
-     }*/
-
-    time_t start = clock();
-    for (int i = 0; i < 50000; ++i) {
-        linkedList.push(i);
+    linkedList.push(0);
+    linkedList.push(1);
+    linkedList.push(2);
+    linkedList.push(3);
+    linkedList.push(4);
+    linkedList.push(5);;
+    linkedList.insert(2, 9);
+    linkedList.insert(linkedList.size(), 0);
+    linkedList.insert(0, 10);
+    for (int i = 0; i < linkedList.size(); ++i) {
+        __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", linkedList.get(i));
     }
-    time_t end = clock();
+    linkedList.remove(2);
 
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", (end - start) / CLOCKS_PER_SEC);
+    for (int i = 0; i < linkedList.size(); ++i) {
+        __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", linkedList.get(i));
+    }
+//    delete(linkedList);
+
+//    time_t start = clock();
+//    for (int i = 0; i < 50000; ++i) {
+//        linkedList.push(i);
+//    }
+//    time_t end = clock();
+//
+//    __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", (end - start) / CLOCKS_PER_SEC);
 //    for (int i = 0; i < linkedList.size(); ++i) {
 //        __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", linkedList.get(i));
 //    }
